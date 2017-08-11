dbb8a12b2429797f1908ef1d8b861badeab01f1f
Tobias Gladh <tobias.gladh@holmgrensbil.se>
2017-08-11 22:31:27 +0200

    Base for startpage

---
+++ src/EPiStarterSite/Features/Start/Models/StartPage.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace EPiStarterSite.Features.Start.Models
+{
+    [ContentType(DisplayName = "StartPage", GUID = "9067bef7-af5f-442a-833f-9501fb4f5c19", Description = "")]
+    public class StartPage : PageData
+    {
+        
+    }
+}
