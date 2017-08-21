497813171f0ad02ac36326dabaae2fb66f754f60
Tobias Gladh <tobias.gladh@holmgrensbil.se>
2017-08-21 15:59:48 +0200

    LayoutModel and DependencyResolver

---
+++ src/EPiStarterSite/Features/Shared/Models/SiteWebViewPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using EPiServer.ServiceLocation;
+
+namespace EPiStarterSite.Features.Shared.Models
+{
+
+    public class SiteWebViewPage : WebViewPage
+    {
+        public Injected<LayoutModel> LayoutModel { get; set; }
+
+        public override void Execute() { }
+    }
+
+    public abstract class SiteWebViewPage<T> : WebViewPage<T>
+    {
+        public Injected<LayoutModel> LayoutModel { get; set; }
+
+        public override void Execute() { }
+    }
+
+}
