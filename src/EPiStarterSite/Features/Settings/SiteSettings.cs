cbd65a14fbc4ce6c89c467727b424d7d28ee50a9
Tobias Gladh <tobias.gladh@holmgrensbil.se>
2017-08-23 08:51:08 +0200

    SettingsPage and Singleton implementation

---
+++ src/EPiStarterSite/Features/Settings/SiteSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiStarterSite.Features.Settings.Models;
+
+namespace EPiStarterSite.Features.Settings
+{
+    public sealed class SiteSettings
+    {
+        private static readonly object Padlock = new object();
+        private readonly IContentLoader _contentLoader;
+
+        private SiteSettings()
+        {
+            _contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var contentEvents = ServiceLocator.Current.GetInstance<IContentEvents>();
+            contentEvents.PublishedContent += contentEvents_PublishedContent;
+        }
+
+        private void contentEvents_PublishedContent(object sender, ContentEventArgs e)
+        {
+            if (!(e.Content is SettingsPage)) return;
+            var settingPage = _contentLoader.GetDescendents(ContentReference.RootPage).Select(_contentLoader.Get<SettingsPage>).OfType<SettingsPage>().FirstOrDefault();
+            _settingsPage = settingPage;
+        }
+
+        private static SiteSettings _instance;
+        public static SiteSettings Instance
+        {
+            get
+            {
+                lock (Padlock)
+                {
+                    return _instance ?? (_instance = new SiteSettings());
+                }
+            }
+        }
+
+        private static SettingsPage _settingsPage;
+        public SettingsPage SettingsPage => _settingsPage ?? (_settingsPage = _contentLoader
+                                                .GetDescendents(ContentReference.RootPage).Select(_contentLoader.Get<SettingsPage>).OfType<SettingsPage>().FirstOrDefault());
+    }
+}
---
+++ src/EPiStarterSite/Infrastructure/Initialization/SingletonTypeInitializationModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using EPiStarterSite.Infrastructure.Attributes;
+
+namespace EPiStarterSite.Infrastructure.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class SingletonTypeInitializationModule : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            ServiceLocator.Current.GetInstance<IContentEvents>().CreatedContent += Instance_CreatedContent;
+            ServiceLocator.Current.GetInstance<IContentEvents>().DeletedContent += Instance_DeletedContent;
+        }
+
+        private static void Instance_DeletedContent(object sender, DeleteContentEventArgs e)
+        {
+            var contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            var contentTyperepository = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+
+            foreach (var cr in e.DeletedDescendents)
+            {
+                var content = contentRepository.Get<IContent>(cr);
+
+                if (content == null) continue;
+                var contentType = contentTyperepository.Load(content.ContentTypeID);
+
+                var attributes = Attribute.GetCustomAttributes(contentType.ModelType);
+                foreach (var attr in attributes)
+                {
+                    if (!(attr is SingletonAttribute)) continue;
+                    var writableContentType = (ContentType)contentType.CreateWritableClone();
+                    writableContentType.IsAvailable = true;
+                    contentTyperepository.Save(writableContentType);
+                }
+            }
+        }
+
+        private static void Instance_CreatedContent(object sender, ContentEventArgs e)
+        {
+            var repository = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+
+            if (repository == null) return;
+            var contentType = repository.Load(e.Content.ContentTypeID);
+
+            var attributes = Attribute.GetCustomAttributes(contentType.ModelType);
+
+            foreach (var attr in attributes)
+            {
+                if (!(attr is SingletonAttribute)) continue;
+                var writableContentType = (ContentType)contentType.CreateWritableClone();
+                writableContentType.IsAvailable = false;
+                repository.Save(writableContentType);
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            ServiceLocator.Current.GetInstance<IContentEvents>().CreatedContent -= Instance_CreatedContent;
+            ServiceLocator.Current.GetInstance<IContentEvents>().DeletedContent -= Instance_DeletedContent;
+        }
+    }
+}
